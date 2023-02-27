285e5343befd671f797e3f82c33f69d4f786405a
Korg X5 Synth <11878913+eksffa@users.noreply.github.com>
2023-02-27 14:16:50 -0500

    adjust comments and language to en_US as per req

--- QED:L-from-purechaos.py
+++ QED:L-from-purechaos.py
@@ -3,21 +3,20 @@
 # FIU Department of Mechanical and Materials Engineering
 # http://mme.fiu.edu
 # -
-# Pra encontrar a densilidade de Langarian no QED do Feynmann temos:
-# 			L = -(1/4) F^μν F_μν + ψ^† (iγ^μ ∂_μ - m) ψ - e ψ^† γ^μ A_μ ψ
-# Toy model para estimar L a partir do puro caos (doi:10.1063/1.3597793, doi:10.1103/PhysRevApplied.3.054004)
+# Finding the Langarian density on QED as per Feynmann's we shall:
+#       L = -(1/4) F^μν F_μν + ψ^† (iγ^μ ∂_μ - m) ψ - e ψ^† γ^μ A_μ ψ
+# This is a toy model to estimate L from pure chaotic randomness (doi:10.1063/1.3597793, doi:10.1103/PhysRevApplied.3.054004)
 
 import random # https://pypi.org/project/quantumrandom/ quantumrandom.binary()[N]
 from sympy import symbols, Matrix
 
 F_mu_nu, psi_dag, gamma_mu, d_mu, m, e, A_mu = symbols('F_mu_nu psi_dag gamma_mu d_mu m e A_mu')
 
+# L = -(1/4) F^μν F_μν + ψ^† (iγ^μ ∂_μ - m) ψ - e ψ^† γ^μ A_μ ψ
 L = - (1/4) * F_mu_nu * F_mu_nu + psi_dag * (1j * gamma_mu * d_mu - m) * psi_dag - e * psi_dag * gamma_mu * A_mu * psi_dag
 
-# L = -(1/4) F^μν F_μν + ψ^† (iγ^μ ∂_μ - m) ψ - e ψ^† γ^μ A_μ ψ
-
 L = L.subs([(F_mu_nu, Matrix([[2, 3], [4, 5]])), (psi_dag, Matrix([[1, 2], [3, 4]])),
-	(gamma_mu, Matrix([[1, 0], [0, -1]])), (d_mu, Matrix([[1, 0], [0, 1]])), (m, 3), (e, 1), (A_mu, Matrix([[1, 0], [0, 1]]))])
+    (gamma_mu, Matrix([[1, 0], [0, -1]])), (d_mu, Matrix([[1, 0], [0, 1]])), (m, 3), (e, 1), (A_mu, Matrix([[1, 0], [0, 1]]))])
 
 def estima_1_sobre_137_0359(n):
     soma = 0
@@ -25,8 +24,9 @@ def estima_1_sobre_137_0359(n):
         soma += 1 / random.uniform(1, 137.0359) # quantumrandom.uint16.uniform(1,137.0359)
     return soma / n
 
-# Gerar uma estimativa de 1/137.0359 usando 10K amostras
-# No universo de brinquedo vai ser preciso mais que 10K haha, um pouco mais ooo Leonidas
+# Estimate 1/137.0359 from 10K samples
+# Adjust sample ratio before running the toy model for serious,
+# as it will will require way more samples, way more Leonidas
 estimativa = estima_1_sobre_137_0359(10000)
 
 def calcular_precisao(L, estimativa):
@@ -36,9 +36,5 @@ print("L vale: {:.10f}".format(L))
 
 precisao = calcular_precisao(L, estimativa)
 
-print("Valor estimado de 1/137.0359: {:.10f}".format(estimativa))
-print("Precisão da estimativa para L = 1/137: {:.6f}%".format(precisao * 100))
-
-
-
-
+print("Estimate val for 1/137.0359: {:.10f}".format(estimativa))
+print("Estimate precision for L = 1/137: {:.6f}%".format(precisao * 100))
